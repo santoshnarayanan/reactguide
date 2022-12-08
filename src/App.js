184be1209b8146b2f987326e3dfe1eee07c8b115
Santosh Narayanan <santosh33@gmail.com>
2022-12-08 20:13:50 +0100

    New Component Added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import ExpenseItem from './components/ExpenseItem';
 
 function App() {
   return (
     <div>
       <h2>Lets get started</h2>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
