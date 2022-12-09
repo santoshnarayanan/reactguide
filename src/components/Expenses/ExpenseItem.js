896c6ae268bba2dd758b331f6149066e23323d9f
Santosh Narayanan <santosh33@gmail.com>
2022-12-09 23:31:44 +0100

    How UseState Hook works

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -6,6 +6,7 @@ import Card from '../UI/Card';
 const ExpenseItem = (props)=> {
 
     const [title,setTitle] = useState(props.title);
+    console.log('ExpenseItem is evaluated');
   
     const clickHandler = ()=>{
         setTitle('Updated!!');
